52d3fcd55e9935ec23fc9e1a83d85c90b0bd5554
adamfodor10 <adamfodor10@gmail.com>
2023-10-30 23:15:04 +0100

    Solve the Javascript 06 exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear;
+    let modulo4 = year % 4;
+    let modulo100 = year % 100;
+    let modulo400 = year % 400;
+    if (modulo4 === 0) {
+        if (modulo100 === 0) {
+            if (modulo400 === 0) {
+                isLeapYear = true;
+            } else {
+                isLeapYear =false;
+            }
+        } else {
+            isLeapYear = true;
+        }
+    } else {
+        isLeapYear = false;
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
