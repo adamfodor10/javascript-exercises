93ffe137f67a335d778b33fd3f469b7fcc614e9f
adamfodor10 <adamfodor10@gmail.com>
2023-10-30 21:04:33 +0100

    Solve the Javascript 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(Array) {
+    //console.log(Array);
+    let index;
+    for (let i = 0; i < arguments.length - 1; i++) {
+        index = Array.indexOf(arguments[i + 1]);
+        if (index === -1) {
+            continue;
+        }
+        //console.log(Array.indexOf(arguments[i + 1]));
+        Array.splice(index, 1);
+        //console.log(Array);
+    }
+    return Array;
 };
+
+removeFromArray([11, 22, 33, 44], 44, 11);
+//console.log(removeFromArray([1, 5, 3, 5], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
