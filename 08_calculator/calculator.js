c734e350afd53e34ebfdb8a89d0345207f5d8454
adamfodor10 <adamfodor10@gmail.com>
2023-11-22 20:04:43 +0100

    Solve the Javascript 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+
+  array.forEach(element => {
+    sum += element;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
 
+  array.forEach(element => {
+    product *= element;
+  });
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let result = 1;
+
+	for (let i = 1; i <= factorial; i++) {
+    result *= i;
+  }
+  
+  return result;
 };
 
 // Do not edit below this line
