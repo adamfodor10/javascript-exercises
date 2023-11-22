a9b8aaacf6b3e9cc6c31fa2b5219200b352886af
adamfodor10 <adamfodor10@gmail.com>
2023-11-22 20:45:17 +0100

    Solve the Javascript 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let onlyLettersArray = string.split('').filter(char => /[a-z]/.test(char));
+    let reversedArray = onlyLettersArray.slice().reverse();
+    return JSON.stringify(onlyLettersArray)===JSON.stringify(reversedArray);
 };
 
 // Do not edit below this line
