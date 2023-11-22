cb530a17f136ec8a917a9a97184b785c878ff01f
adamfodor10 <adamfodor10@gmail.com>
2023-11-22 21:42:44 +0100

    Solve the Javascript 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let age = [];
+    array.forEach(item => {
+        if (item.yearOfDeath === undefined) {
+            item.yearOfDeath = new Date().getFullYear();
+        }
+        age.push(item.yearOfDeath - item.yearOfBirth)
+    });
 
+    let oldestAge = Math.max(...age);
+    let indexOfOldest = age.findIndex(age => age === oldestAge );
+    return array[indexOfOldest];
 };
 
 // Do not edit below this line
