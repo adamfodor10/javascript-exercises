3a1e26da054a9feab6fc8e8249ba63afdd7b8e02
adamfodor10 <adamfodor10@gmail.com>
2023-11-22 21:10:26 +0100

    Solve the Javascript 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequenceIndex) {
+    sequenceIndex = Number(sequenceIndex);
+    if ((sequenceIndex === NaN) || (sequenceIndex <= 0)) return 'OOPS';
+    else if ((sequenceIndex === 1) || (sequenceIndex === 2)) return 1;
+    else {
+        let sequenceMember;
+        let sequenceMemberMinus1 = 1;
+        let sequenceMemberMinus2 = 1;
+        for (let i = 3; i <= sequenceIndex; i++) {
+            sequenceMember = sequenceMemberMinus1 + sequenceMemberMinus2;
+            sequenceMemberMinus2 = sequenceMemberMinus1;
+            sequenceMemberMinus1 = sequenceMember;
+        }
+        return sequenceMember;
+    }
 };
 
 // Do not edit below this line
